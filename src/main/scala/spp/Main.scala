5b7577a7929a1602fc8ffcee3822939f8de2646e
Benoit <benoit.maillard@epfl.ch>
2020-02-19 15:18:14 +0100

    Complete main file with arguments parsing and pipeline

--- src/main/scala/spp/Main.scala
+++ src/main/scala/spp/Main.scala
@@ -1,7 +1,28 @@
 package spp
+import utils._
+import spp.parsing.Lexer
+import java.io.File
 
 object Main {
     def main(args: Array[String]): Unit = {
-        
+        val context = Context(new Reporter, args.toList)
+        val pipeline = Lexer
+
+        try {
+            if (context.files.isEmpty) context.reporter.fatal("No source files provided")
+            else {
+                pipeline.run(context)(
+                    context.files.map(name => {
+                        val file = new File(name)
+                        if (file.exists()) file
+                        else context.reporter.fatal(s"File $name not found") 
+                    })
+                )
+            }
+            
+            context.reporter.terminateIfErrors()
+        } catch {
+            case AmycFatalError(msg) => sys.exit(1)
+        }
     }
 }
