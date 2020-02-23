b0e1c3da056013838b294271387231c69bb01a20
Benoit <benoit.maillard@epfl.ch>
2020-02-23 13:29:56 +0100

    Add basic tokens

--- src/main/scala/spp/structure/Tokens.scala
+++ src/main/scala/spp/structure/Tokens.scala
@@ -1,6 +1,33 @@
 package spp.structure
 
+import spp.utils.Positioned
+
 object Tokens {
-    sealed trait Token
+    sealed abstract class Token extends Positioned with Product {
+        override def toString(): String = {
+            productPrefix + productIterator.mkString("(", ",", ")")
+        }
+    }
     sealed trait TokenKind
+
+    // TODO intermediary tokens for indent/dedent
+    final case class Newline() extends Token
+    final case class Indent() extends Token
+    final case class Dedent() extends Token
+
+    final case class Keyword(name: String) extends Token
+    final case class Identifier(name: String) extends Token
+
+    final case class StringLiteral(value: String) extends Token
+    final case class FormatStringLiteral(value: String) extends Token
+    final case class BytesLiteral(value: String) extends Token
+    
+    final case class IntLiteral(value: Int) extends Token
+    final case class FloatLiteral(value: Float) extends Token
+    final case class ImaginaryLiteral(value: Float) extends Token
+    
+    final case class Operator(op: String) extends Token
+    final case class Delimiter(del: String) extends Token
+
+    final case class Space() extends Token
 }
