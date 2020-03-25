399d0e311d33eadd7361600b61c385e96a697c4c
Benoit <benoit.maillard@epfl.ch>
2020-03-25 10:42:26 +0100

    Add basic tree types

--- src/main/scala/spp/structure/AbstractSyntaxTree.scala
+++ src/main/scala/spp/structure/AbstractSyntaxTree.scala
@@ -1,6 +1,16 @@
 package spp.structure
 
+import spp.utils.Positioned
+
 object AbstractSyntaxTree {
-  case class Program()
+  trait Tree extends Positioned
+  case class Module(body: Seq[Statement]) extends Tree
+
+  trait Statement extends Tree
+  case class Assign(left: Seq[Seq[Expr]], value: Seq[Expr]) extends Statement
   
+  trait Expr extends Tree
+  case class Name(namespace: Option[String], name: String) extends Expr
+  case class TernaryExpr(condition: Expr, ifValue: Expr, elseValue: Expr) extends Expr
+  case class BinaryExpr(operator: String, left: Expr, right: Expr) extends Expr
 }
