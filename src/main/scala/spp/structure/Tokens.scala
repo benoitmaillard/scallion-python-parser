3273047776870ccc6f9d82c5d761d02896adc39c
Benoit <benoit.maillard@epfl.ch>
2020-03-25 10:44:03 +0100

    Add token classes

--- src/main/scala/spp/structure/Tokens.scala
+++ src/main/scala/spp/structure/Tokens.scala
@@ -9,7 +9,6 @@ object Tokens {
       productPrefix + productIterator.mkString("(", ",", ")")
     }
   }
-  sealed trait TokenKind
   
   // TODO intermediary tokens for indent/dedent
   final case class Newline() extends Token
@@ -20,7 +19,7 @@ object Tokens {
   final case class Identifier(name: String) extends Token
   
   final case class StringLiteral(prefix: Option[String], value: String) extends Token
-  final case class FormatStringLiteral(value: String) extends Token
+  // final case class FormatStringLiteral(value: String) extends Token
   final case class BytesLiteral(value: String) extends Token
   
   final case class IntLiteral(value: BigInt) extends Token
@@ -30,11 +29,29 @@ object Tokens {
   final case class Operator(op: String) extends Token
   final case class Delimiter(del: String) extends Token
   
-  final case class Space() extends Token
-  final case class Comment() extends Token
-
-  final case class ErrorToken(msg: String) extends Token
   final case class EOF() extends Token
   
+  // Intermediary tokens
+  final case class Space() extends Token
+  final case class Comment() extends Token
+  final case class ErrorToken(msg: String) extends Token
   final case class PhysicalIndent(length: Int) extends Token
+}
+
+object TokenClasses {
+  sealed abstract class TokenClass(val display: String) extends Positioned {
+
+  }  
+  final case object NewlineClass extends TokenClass("\n")
+  final case object IndentClass extends TokenClass("<indent>")
+  final case object DedentClass extends TokenClass("<dedent>")
+  final case class KeywordClass(value: String) extends TokenClass(value)
+  final case object NameClass extends TokenClass("<name>")
+  final case object LiteralClass extends TokenClass("<literal>")
+
+  final case class OperatorClass(value: String) extends TokenClass(value)
+  final case class DelimiterClass(value: String) extends TokenClass(value)
+  final case object EOFClass extends TokenClass("<EOF>")
+
+  final case object OtherClass extends TokenClass("<other>")
 }
