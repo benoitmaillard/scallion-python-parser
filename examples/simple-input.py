4f43725e32a6fecc6151dba79ec2fc300b541d1c
Benoit <benoit.maillard@epfl.ch>
2020-04-01 23:21:35 +0200

    Add example

---
+++ examples/simple-input.py
@@ -0,0 +1,12 @@
+x, y, z = y, s, d = a, b, c; x = y
+a = b;
+x = y if a else z
+x = a and b
+x = not x is not b
+x = x < y < z == a
+x = 1 + 4j
+x = True
+a.x = 5
+a = some_call(x, y)
+a = x[5:3:4]
+x = some_call()[4].some_other_call(x, y:=3, a=y, *args, **kwargs)()[i]
