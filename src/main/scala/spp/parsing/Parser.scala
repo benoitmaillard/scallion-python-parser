2976f64b4968e0470a28cfb748d90834f3f1c569
Benoit <benoit.maillard@epfl.ch>
2020-05-27 17:56:44 +0200

    Complete f-string implementation

--- src/main/scala/spp/parsing/Parser.scala
+++ src/main/scala/spp/parsing/Parser.scala
@@ -753,69 +753,8 @@ object Parser extends Syntaxes with ll1.Parsing with Operators with ll1.Debug wi
     })
 
   val parser = LL1(module)
+  val stringParser = LL1(test)
   val printer = PrettyPrinter(module)
-
-  def parseString(tokens: Iterator[Token], sl: StringLiteral): JoinedStr = {
-    def asPos[A](s: Syntax[Token]) = s.map({
-      case t => t.position
-    })
-
-    // gets the start of some irrelevant content (string content)
-    lazy val unrelevant: Syntax[Int] = many1(elem(NameClass)) map ({
-      case t => t.head.position.col
-    })
-
-    lazy val fString: Syntax[JoinedStr] = many(unrelevant ~ elem(DelimiterClass("{")) ~ replacementField ~ elem(DelimiterClass("}"))) ~ eof.skip map ({
-      case s => JoinedStr(s.foldLeft(Seq[Expr]()) {
-        case (acc, startPos ~ ob ~ replace ~ cb) => {
-          val str = sl.value.substring(startPos, ob.position.col)
-          val strConstant = StringLiteralParser.parse(StringLiteral("", sl.delimiter, str))
-          val formattedValue = replace
-          acc ++ Seq(strConstant, formattedValue)
-        }
-      })
-    })
-
-    lazy val replacementField: Syntax[FormattedValue] =
-      fExpression ~ opt(opU("!").skip ~ conversion) ~ opt(delU(":").skip ~ formatSpec) map ({
-        case e ~ optConv ~ optFormat => FormattedValue(e, optConv, optFormat)
-      })
-    // NOTE in docs.python.org `conditional_expression` is actually `test`
-    lazy val fExpression: Syntax[Expr] = test // TODO this is incomplete
-    lazy val conversion: Syntax[Char] = nameString map ({
-      case n => n.head // TODO this is really yolo
-    })
-
-    // either a StringConstant or a JoinedStr
-    lazy val formatSpec: Syntax[JoinedStr] = nameString map ({
-      n => JoinedStr(Seq(StringConstant(n)))
-    })
-
-    val stringParser = LL1(fString)
-
-    if (!fString.isLL1) {
-      debug(fString)
-      
-      throw new Error("Not LL1 !!!")
-    } else {
-      println("Syntax is LL1!")
-    }
-    
-    //tokens.foreach(println(_))
-    val res = stringParser(tokens) match {
-      case LL1.Parsed(value, rest) => value
-      case LL1.UnexpectedToken(token, rest) => {
-        println(token)
-        println(token.position)
-        throw new Error("Invalid token !!!")
-      }
-      case LL1.UnexpectedEnd(rest) => {
-        println(f"Rest : $rest")
-        throw new Error("Unexpected end !!!!")
-      } 
-    }
-    res
-  }
   
   def apply(ctx: Context, tokens: Iterator[Token]): Module = {
     if (!module.isLL1) {
@@ -841,6 +780,14 @@ object Parser extends Syntaxes with ll1.Parsing with Operators with ll1.Debug wi
     println(pretty)
 
     res
+  }
+
+  def parseExpr(tokens: Iterator[Token]): Expr = {
+    stringParser(tokens) match {
+      case LL1.Parsed(value, rest) => value
+      case LL1.UnexpectedToken(token, rest) => throw new Error(f"Invalid token $token")
+      case LL1.UnexpectedEnd(rest) => throw new Error("Invalid end")
+    }
   }
 
   def unapply(value: Module): Option[String] = {
--- src/main/scala/spp/parsing/StringLiteralParser.scala
+++ src/main/scala/spp/parsing/StringLiteralParser.scala
@@ -1,19 +1,98 @@
 package spp.parsing
 
 import spp.structure.Tokens.StringLiteral
-import spp.structure.AbstractSyntaxTree._
-import spp.lexer.StringDecoder._
+import spp.structure.AbstractSyntaxTree.{Expr, StringConstant, BytesConstant, JoinedStr, FormattedValue}
+import spp.lexer.StringDecoder.decode
 import spp.lexer.Lexer
+import spp.lexer.StringDecoder
+import spp.structure.Tokens._
+
+import scala.util.{Try, Success, Failure}
 
 object StringLiteralParser {
   def parse(sl: StringLiteral): Expr = {
     val prefix = sl.prefix.toLowerCase
-    if (prefix.contains('f')) JoinedStr(parseFormattedStr(sl.value))
+    if (prefix.contains('f')) JoinedStr(parseFormattedStr(sl.prefix, 0, sl.value)._1)
     else if (prefix.contains('b')) BytesConstant(decode(prefix, sl.value).get)
     else StringConstant(decode(prefix, sl.value).get)
   }
 
-  def parseFormattedStr(str: String): Seq[Expr] = ???
+  // TODO: should probably return the position of the end of expr (or the remaining str ?)
+  def parseFormattedStr(prefix: String, level: Int, str: String): (Seq[Expr], String) = {
+    val cstPart = StringDecoder.decode(prefix, str).get
+    val tailPart = str.drop(cstPart.length)
 
-  def extractExprPart(str: String): String = ???
+    // TODO: in the case were we encounter a '}', we must check that we are at level 0
+    if (tailPart.isEmpty) (cstPartSeq(cstPart), "")
+    else if (tailPart.head == '}') (cstPartSeq(cstPart), tailPart.drop(1)) // we drop the '}'
+    else {
+      val (fValue, remaining) = parseFromExpr(prefix, level, tailPart.drop(1)) // we drop the '{'
+      (cstPartSeq(cstPart) :+ fValue, remaining)
+
+      val (exprsAfter, remainingAfter) = parseFormattedStr(prefix, level, remaining)
+      ((cstPartSeq(cstPart) :+ fValue) ++ exprsAfter, remainingAfter)
+    }
+  }
+
+  def cstPartSeq(part: String): Seq[Expr] =
+    if (!part.isEmpty) Seq(StringConstant(part))
+    else Seq()
+
+  // str starts with first char after '{'
+  def parseFromExpr(prefix: String, level: Int, str: String): (FormattedValue, String) = {
+    // TODO: should check if string is empty (missing })
+    val (exprTokens, afterExpr) = extractExpr(str)
+
+    // TODO: should check if expression is empty (and valid, think of lambda case)
+    val expr = Parser.parseExpr(exprTokens)
+    
+    val (conversion, afterConversion) = extractConversion(afterExpr)
+    val (format, afterFormat) = extractFormat(prefix, level, afterConversion)
+
+    (FormattedValue(expr, conversion, format), afterFormat.drop(1)) // we drop terminating '}'
+  }
+
+  def extractConversion(str: String): (Option[Char], String) = str.toSeq match {
+    case '!' +: c +: tail => (Some(c), tail.mkString)
+    case _ => (None, str)
+  }
+
+  def extractFormat(prefix: String, level: Int, str: String): (Option[Expr], String) = str.toSeq match {
+    case ':' +: format => {
+      val (exprs, remaining) = parseFormattedStr(prefix, level + 1, format.mkString)
+      (Some(JoinedStr(exprs)), remaining)
+    }
+    case _ => (None, str)
+  }
+  
+  // Finds the tokens of the expression and the remaining input from the first char
+  // after the expression (this would be either '!', ':' or '}')
+  def extractExpr(str: String): (Iterator[Token], String) = {
+    val tokens = Lexer.applyString(str).toList
+
+    // we are looking for 3 things : !, :, } at level 0
+    // TODO: add surrounding parentheses !!! (maybe rather in extractExpr)
+    val (exprTokens, tokenAfter) = findExprEnd(tokens, List()).get
+
+    (exprTokens.iterator, str.substring(tokenAfter.position.col))
+  }
+
+  def findExprEnd(tokens: List[Token], stack: List[String]): Try[(List[Token], Token)] = {
+    def findExprEndAcc(tokens: List[Token], stack: List[String], acc: List[Token]): Try[(List[Token], Token)] = tokens match {
+      case (t@Delimiter(del@("{" | "[" | "("))) :: tail =>
+        findExprEndAcc(tail, del :: stack, t :: acc)
+      case (t@Delimiter(del@("}" | "]" | ")"))) :: tail => stack match {
+        case `del` :: stackTail => findExprEndAcc(tail, stackTail, t :: acc)
+        case Nil => if (del == "}") Success((acc, t)) else Failure(new Error(f"Unmatched $del")) 
+        case _ => Failure(new Error(f"Unmatched $del"))
+      }
+      case (t@Delimiter(":" | "!" )) :: tail => stack match {
+        case Nil => Success((acc, t))
+        case _ => findExprEndAcc(tail, stack, t :: acc)
+      }
+      case other :: tail => findExprEndAcc(tail, stack, other :: acc)
+      case Nil => Failure(new Error(f"Unclosed format value"))
+    }
+    findExprEndAcc(tokens, stack, List())
+  }
 }
