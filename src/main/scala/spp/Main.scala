435ce169732d4dec5229421768229dafd9bbb926
Benoit <benoit.maillard@epfl.ch>
2020-06-09 10:07:07 +0200

    Add tokenize, parse and compare options in main program

--- src/main/scala/spp/Main.scala
+++ src/main/scala/spp/Main.scala
@@ -2,17 +2,28 @@ package spp
 import utils._
 import spp.parsing._
 import spp.lexer._
+import spp.structure.TreeSerializer._
+
 import java.io.File
 
+import org.json4s._
+import org.json4s.native.JsonMethods._
+import org.json4s.JsonDSL._
+
 object Main {
-  def main(args: Array[String]): Unit = {
-    try {
-      
-      val tokens = Lexer(args.toList.head)
+  def main(args: Array[String]): Unit = args(0) match {
+    case "tokenize" => Lexer(args(1)).foreach(println(_))
+    case "parse" => {
+      val tokens = Lexer(args(1))
       val tree = Parser(tokens)
-      println(tree)      
-    } catch {
-      case AmycFatalError(msg) => sys.exit(1)
+
+      println(tree)
+    }
+    case "compare" => {
+      val tokens = Lexer(args(1))
+      val tree = Parser(tokens)
+      
+      compare(args(2), tree)
     }
   }
 }
