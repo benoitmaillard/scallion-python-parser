f2c30d9b7a070b0746ae554b022fe4ac5d92121e
Benoit <benoit.maillard@epfl.ch>
2020-06-17 22:06:04 +0200

    Add code for performance analysis

--- src/main/scala/spp/Main.scala
+++ src/main/scala/spp/Main.scala
@@ -3,12 +3,14 @@ import utils._
 import spp.parsing._
 import spp.lexer._
 import spp.structure.TreeSerializer._
+import scala.util.{Try, Success, Failure}
 
 import java.io.File
 
 import org.json4s._
 import org.json4s.native.JsonMethods._
 import org.json4s.JsonDSL._
+import spp.structure.AbstractSyntaxTree.Module
 
 object Main {
   def main(args: Array[String]): Unit = args(0) match {
@@ -35,5 +37,56 @@ object Main {
       
       compare(args(2), tree)
     }
+
+    case "parsedir" => {
+      parseDir(args(1))
+    }
+  }
+
+  def parse(file: String): Option[(Double, Double)] = Try {
+    val start = System.nanoTime
+    val tokens = Lexer(file)
+
+    val lexerT = (System.nanoTime - start) * 1.0E-6
+    val tree = Parser(tokens)
+    val totalT = (System.nanoTime - start) * 1.0E-6
+    (lexerT, totalT)
+  } match {
+    case Success((lexerT, totalT)) => Some(lexerT, totalT)
+    case Failure(e) => println(e.getMessage()); None
+  }
+
+  def parseDir(dirPath: String) = {
+    val dir = new File(dirPath)
+    val files = recursiveListFiles(dir).map(_.getAbsolutePath())
+    val pythonFiles = files.filter(_.matches(""".*\.py"""))
+    println(f"Starting parsing of ${pythonFiles.size} files")
+
+    //pythonFiles.foreach(println(_))
+    val results = pythonFiles.map(parse(_))
+
+    val successes = results.collect{
+      case Some((lexer, total)) => (lexer, total)
+    }
+
+    val failures = results.collect{
+      case None =>
+    }.size
+
+    val lexerT = successes.map(_._1).sum
+    val totalT = successes.map(_._2).sum
+
+    println("Results")
+    println(f"- Failures : $failures")
+    println(f"- Success ${successes.size}")
+    println("Measured execution time")
+    println(f"- Lexer : $lexerT")
+    println(f"- Total : $totalT")
+  }
+  
+  // code taken from https://stackoverflow.com/questions/2637643/how-do-i-list-all-files-in-a-subdirectory-in-scala
+  def recursiveListFiles(f: File): Array[File] = {
+    val these = f.listFiles
+    these ++ these.filter(_.isDirectory).flatMap(recursiveListFiles)
   }
 }
